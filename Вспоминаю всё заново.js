1d9aa43a7e2c4fa5ba9093649739315b5e18eec3
Vanya <babkov900@gmail.com>
2023-03-18 17:06:01 +0200

    add changeHeight() function and add styles.css

--- Вспоминаю всё заново.js
+++ Вспоминаю всё заново.js
@@ -1,5 +1,15 @@
 document.body.style.background = 'red';
 
+const step = 10;
+let currentHeight = 0;
+function changeHeight() {
+    const elements = document.getElementsByClassName('changeble-height');
+    currentHeight += step;
+    for(let i = 0; i < elements.length; i++) {
+        
+        elements[i].style.height = `${currentHeight}px`;
+    }
+}
 
 
 
