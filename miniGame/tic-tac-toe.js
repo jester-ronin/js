f3a022668d4b31bec2d5a8a0c177771f7092e7d0
Vanya <babkov900@gmail.com>
2023-03-22 17:25:34 +0200

    pianoPosition

--- miniGame/tic-tac-toe.js
+++ miniGame/tic-tac-toe.js
@@ -2,32 +2,40 @@ let firstLine = document.querySelectorAll('.field1');
 let secondLine = document.querySelectorAll('.field2');
 let thierdLine = document.querySelectorAll('.field3');
 
+let currentColor = 'black';
+
 firstLine.forEach(element => {
-   element.style.background = 'grey';
+    element.style.background = 'grey';
 })
 
 secondLine.forEach(element => {
     element.style.background = 'grey';
- })
+})
 
 thierdLine.forEach(element => {
     element.style.background = 'grey';
- })
+})
 
 
- function firstPlayerColor(event) {
-    event.target.style.background = 'black';
-
-}
-
-function secondPlayerColor(event) {
-    event.target.style.background = 'white';
-
+function toggleColor(event) {
+    if (currentColor === 'black') {
+        event.target.style.background = 'black';
+        currentColor = 'white';
+    } else {
+        event.target.style.background = 'white';
+        currentColor = 'black';
+    }
 }
 
 firstLine.forEach(element => {
-    element.addEventListener('click', firstPlayerColor);
-    if(element.style.background === 'black') {
-    element.addEventListener('click', secondPlayerColor);
-    }
-  });
+    element.addEventListener('click', toggleColor);
+});
+
+secondLine.forEach(element => {
+    element.addEventListener('click', toggleColor);
+});
+
+thierdLine.forEach(element => {
+    element.addEventListener('click', toggleColor);
+});
+
